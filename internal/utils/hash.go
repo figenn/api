47f2b243c35f41a0bc5fa7d19e76c71eeb9f81bf
Alexandre Dissi <alexandredissi@icloud.com>
2025-03-18 02:33:37 +0100

    Replace bcrypt with Argon2 for password hashing and comparison

--- internal/utils/hash.go
+++ internal/utils/hash.go
@@ -1,13 +1,45 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/argon2"
+)
 
 func HashPassword(password string) (string, error) {
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(hashedPassword), err
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+
+	hashedPassword := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
+	return base64.RawStdEncoding.EncodeToString(append(salt, hashedPassword...)), nil
 }
 
 func ComparePassword(hashedPassword, password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-	return err == nil
+	decoded, err := base64.RawStdEncoding.DecodeString(hashedPassword)
+	if err != nil {
+		return false
+	}
+
+	salt := decoded[:16]
+	storedHash := decoded[16:]
+
+	computedHash := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
+
+	return compareByteSlices(storedHash, computedHash)
+}
+
+func compareByteSlices(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
